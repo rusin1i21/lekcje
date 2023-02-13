965a8dfc92c7f960f637f8893796803296e7dfd4
rusin1i21 <106950243+rusin1i21@users.noreply.github.com>
2023-02-13 14:12:31 +0100

    Update odwracanie listy.cpp

--- odwracanie listy.cpp
+++ odwracanie listy.cpp
@@ -5,13 +5,51 @@ using namespace std;
 
 int main (int argc, char const *argv[])
 {
-    srand.time(0);
+    srand(time(0));
+    int lost = rand();
+    // cout << lost;
 
-   
-   
+    cout << "podaj rozmiar tablicy";
+
+    int rozmiar;
+    cin >> rozmiar;
+    int tablica[rozmiar]; // int *tablica = new int[rozmiar]
+    for (int i=0 ; i< rozmiar ;i++)
+        tablica[i]=(rand()%10)+1;
+
+    for (int i = 0; i <rozmiar; i++)
+        cout << tablica [i] << " ";
+
+cout << "\n---------------------------------------------------" << endl;
+    
+    for ( int i = 0; i < rozmiar/2 ; i++ )
+        {int tmp = tablica[i];
+        tablica[i] = tablica [rozmiar-i-1];
+        tablica[rozmiar-i-1] = tmp;
 
 
+        }
+    for (int i = 0; i <rozmiar; i++)
+        cout << tablica [i] << " ";
 
+    // usuwanie elementów w środku tablicy
+    int s=0;
+    if ( rozmiar%2==0)
+        s = (rozmiar-1)/2;
+    else
+        s = rozmiar/2;
+    
+    for (;s<rozmiar;s++){
+        tablica[s] = tablica[s+1];
 
+    }
+cout << "\n---------------------------------------------------" << endl;
+    for (int i = 0; i <rozmiar; i++)
+        cout << tablica [i] << " ";
+
+       
+
+    
+    
     return 0;
 }    
